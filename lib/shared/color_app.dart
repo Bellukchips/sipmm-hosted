fed8da5e8857b1fbe6101a1a27d58f7fc1b1cc75
bellukchips <bellukchips@gmail.com>
2023-05-26 13:50:58 +0700

    change primary color

--- lib/shared/color_app.dart
+++ lib/shared/color_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:supercharged/supercharged.dart';
 
 class ColorApp {
-  static Color primaryColor = "00BE2A".toColor();
+  static Color primaryColor = "32613D".toColor();
   static Color yellowDark = 'A68C49'.toColor();
   static Color yellowAccent = 'FFCF51'.toColor();
 }
